183068df53eda280b129edbde6a07f606ee5a70f
Elemnir <ahoward0920@gmail.com>
2015-12-14 16:00:18 -0500

    Created data structures for holding callsite info

--- harnesshooks.cpp
+++ harnesshooks.cpp
@@ -4,17 +4,31 @@
 #include <stdio.h>
 #include <string>
 #include <string.h>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
+struct CallSiteData {
+    CallSiteData() : total(0) {}
+    
+    // The amount of data the callsite accrued
+    size_t total;
 
+    // The list of returned pointers and their sizes given to this callsite
+    vector<pair<void*, size_t> > entries;
+
+};
+
+map<string, CallSiteData> callsites;
 
 void harness_init() {
 }
 
 void harness_recv_event(const char* buf) {
     static vector<string> trace;
+    static size_t amt, t_index;
+    static void *ptr;
 
     if (!strcmp(buf, "AllocStart\n")) {
         trace.clear();
@@ -31,4 +45,5 @@ void harness_recv_event(const char* buf) {
 }
 
 void harness_finalize() {
+
 }
